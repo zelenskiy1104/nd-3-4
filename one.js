38d909774499da701441e3c650d763b1a6ab5683
Сергей Зеленский <zelenskiy1104@gmail.com>
2016-11-15 17:29:19 +0600

    part one

---
+++ one.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const crypto = require('crypto'); 
+
+const input = fs.createReadStream("data.txt");
+const output = fs.createWriteStream("hash.txt");
+
+var hash = crypto.createHash('md5');
+
+//чтобы сравнить 2 значения, в файле и в консольке
+hash.setEncoding('hex');
+
+input.on("end",  () => {
+    hash.end();
+    console.log(hash.read());
+});
+
+input.pipe(hash);
+
+hash.on("finish",  () => {
+    hash.pipe(output);
+});
